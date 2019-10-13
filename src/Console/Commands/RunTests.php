335032704ffc1b4681ce420e0b7ad0eabcaef9fd
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2019-10-13 15:54:31 +0200

    Only stop server when running

--- src/Console/Commands/RunTests.php
+++ src/Console/Commands/RunTests.php
@@ -9,30 +9,31 @@ class RunTests extends Command
 {
     protected $signature = 'test';
     protected $description = 'Runs the test scripts for the project where component-testing is installed';
-
     /** @var Process */
     private $server;
 
     public function handle()
     {
-        if (file_exists(base_path('tests/Browser'))) {
-            $this->script("php artisan dusk:chrome-driver");
+        try {
+            if (file_exists(base_path('tests/Browser'))) {
+                $this->script("php artisan dusk:chrome-driver");
 
-            $this->server = new Process("php -S 0.0.0.0:8000 -t public 2>/dev/null");
-            $this->server->start();
+                $this->server = new Process("php -S 0.0.0.0:8000 -t public 2>/dev/null");
+                $this->server->start();
+            }
+
+            touch(base_path('database/database.sqlite'));
+
+            $this->info("Running phpunit");
+            $this->script("./vendor/bin/phpunit --cache-result --order-by=defects --stop-on-failure");
+
+            if (file_exists(base_path('tests/Browser'))) {
+                $this->info("Running Dusk");
+                $this->script("php artisan dusk --cache-result --order-by=defects --stop-on-failure");
+            }
+        } finally {
+            optional($this->server)->stop();
         }
-
-        touch(base_path('database/database.sqlite'));
-
-        $this->info("Running phpunit");
-        $this->script("./vendor/bin/phpunit --cache-result --order-by=defects --stop-on-failure");
-
-        if (file_exists(base_path('tests/Browser'))) {
-            $this->info("Running Dusk");
-            $this->script("php artisan dusk --cache-result --order-by=defects --stop-on-failure");
-        }
-
-        $this->server->stop();
     }
 
     private function script(string $script)
