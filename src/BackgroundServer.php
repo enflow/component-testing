e1523ae77d7d9c15e850cdbcc357d8272d461d45
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2020-05-12 20:28:57 +0200

    Fix background server

--- src/BackgroundServer.php
+++ src/BackgroundServer.php
@@ -2,26 +2,50 @@
 
 namespace Enflow\Component\Testing;
 
+use Exception;
+
 class BackgroundServer
 {
     private $process;
+    private $pipes = [];
 
     public function start()
     {
-        $pipes = [];
-
         $this->process = proc_open("php -S 0.0.0.0:8000 -t public", [
             ['pipe', 'r'],
             ['pipe', 'w'], // stdout
             ['pipe', 'w'], // stderr
-        ], $pipes, getcwd(), $this->getEnv());
+        ], $this->pipes, getcwd(), $this->getEnv());
+
+        $status = proc_get_status($this->process);
+        if (empty($status['running'])) {
+            throw new Exception("PHP server must be running");
+        }
     }
 
     public function stop()
     {
-        if ($this->process) {
-            proc_terminate($this->process);
+        $status = proc_get_status($this->process);
+        if (empty($status['running'])) {
+            throw new Exception("PHP server must be running");
         }
+
+        //close all pipes that are still open
+        fclose($this->pipes[1]); //stdout
+        fclose($this->pipes[2]); //stderr
+
+        //get the parent pid of the process we want to kill
+        $ppid = $status['pid'];
+
+        //use ps to get all the children of this process, and kill them
+        $pids = preg_split('/\s+/', `ps -o pid --no-heading --ppid $ppid`);
+        foreach ($pids as $pid) {
+            if (is_numeric($pid)) {
+                posix_kill($pid, 9); //9 is the SIGKILL signal
+            }
+        }
+
+        proc_close($this->process);
     }
 
     protected function getEnv(): array
@@ -29,7 +53,7 @@ class BackgroundServer
         $envPairs = [];
         foreach ($this->getDefaultEnv() as $k => $v) {
             if (false !== $v && in_array($k, $this->envToPassthrough())) {
-                $envPairs[] = $k.'='.$v;
+                $envPairs[] = $k . '=' . $v;
             }
         }
 
